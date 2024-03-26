75a400f2c97829a044a164d5f344ff2822603954
jyuch <jyuch@users.noreply.github.com>
2024-03-26 21:26:46 +0900

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use clap::Parser;
 use hickory_client::client::{AsyncClient, ClientHandle};
-
 use hickory_client::udp::UdpClientStream;
 use hickory_server::authority::MessageResponseBuilder;
 use hickory_server::proto::op::{Header, MessageType, OpCode, ResponseCode};
-
-use hickory_server::proto::rr::{IntoName};
+use hickory_server::proto::rr::IntoName;
 use hickory_server::server::{Request, RequestHandler, ResponseHandler, ResponseInfo};
 use hickory_server::ServerFuture;
 use std::net::SocketAddr;
@@ -64,13 +62,9 @@ impl RequestHandler for StubRequestHandler {
                     let upstream = &mut *self.upstream.lock().await;
                     forward_to_upstream(upstream, request, response_handle).await
                 }
-                _op => {
-                    server_not_implement(request, response_handle).await
-                }
+                _op => server_not_implement(request, response_handle).await,
             },
-            MessageType::Response => {
-                server_not_implement(request, response_handle).await
-            }
+            MessageType::Response => server_not_implement(request, response_handle).await,
         };
 
         result.unwrap_or_else(|_e| {
@@ -81,7 +75,11 @@ impl RequestHandler for StubRequestHandler {
     }
 }
 
-async fn forward_to_upstream<R: ResponseHandler>(upstream: &mut AsyncClient, request: &Request, mut response_handle: R) -> anyhow::Result<ResponseInfo> {
+async fn forward_to_upstream<R: ResponseHandler>(
+    upstream: &mut AsyncClient,
+    request: &Request,
+    mut response_handle: R,
+) -> anyhow::Result<ResponseInfo> {
     let response = upstream
         .query(
             request.query().name().into_name().unwrap(),
@@ -103,7 +101,10 @@ async fn forward_to_upstream<R: ResponseHandler>(upstream: &mut AsyncClient, req
     Ok(response_info)
 }
 
-async fn server_not_implement<R: ResponseHandler>(request: &Request, mut response_handle: R) -> anyhow::Result<ResponseInfo> {
+async fn server_not_implement<R: ResponseHandler>(
+    request: &Request,
+    mut response_handle: R,
+) -> anyhow::Result<ResponseInfo> {
     let response = MessageResponseBuilder::from_message_request(request);
     let response_info = response_handle
         .send_response(response.error_msg(request.header(), ResponseCode::NotImp))
